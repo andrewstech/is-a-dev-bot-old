b0ac379de817575eb5145c2ccced8a7b92ed68f4
andrewstech <andrew@win11react.com>
2023-02-11 23:46:38 +0000

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 const JSONdb = require('simple-json-db');
 
 const db = new JSONdb('storage.json');
-const userdb = new JSONdb('storageuser.json');
-const dbemail = new JSONdb('storageemail.json');
-const pairdb = new JSONdb('storagepair.json');
+const userdb = new JSONdb('/is-a-dev/storageuser.json');
+const dbemail = new JSONdb('/is-a-dev/storageemail.json');
+const pairdb = new JSONdb('/is-a-dev/storagepair.json');
 
 
 module.exports = { db, userdb, dbemail, pairdb };
