50b10b40b2d7a2b3f001c68f864b11817a6dd35c
andrewstech <andrew@win11react.com>
2023-02-11 23:47:04 +0000

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const JSONdb = require('simple-json-db');
 
-const db = new JSONdb('storage.json');
+const db = new JSONdb('/is-a-dev/storage.json');
 const userdb = new JSONdb('/is-a-dev/storageuser.json');
 const dbemail = new JSONdb('/is-a-dev/storageemail.json');
 const pairdb = new JSONdb('/is-a-dev/storagepair.json');
